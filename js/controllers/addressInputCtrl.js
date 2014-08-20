c43484ed0841861318b4b00729b4e1d2d9889407
Dan Toivonen <dtoivonen@four51.com>
2014-08-20 09:02:05 -0500

    Update to Awards App

--- js/controllers/addressInputCtrl.js
+++ js/controllers/addressInputCtrl.js
@@ -1,40 +1,47 @@
 four51.app.controller('AddressInputCtrl', ['$scope', '$rootScope', '$location', 'User', 'Address', 'Resources',
-function ($scope, $rootScope, $location, User, Address, Resources) {
-    $scope.save = function() {
-	    $scope.objectExists = false;
-        Address.save(this.address,
-	        function(address) {
-                $rootScope.$broadcast('event:AddressSaved', address);
-                $location.path($scope.return);
-            },
-	        function(ex) {
-	            if (ex.Code.is('ObjectExistsException'))
-	                $scope.objectExists = true;
+    function ($scope, $rootScope, $location, User, Address, Resources) {
+
+        $scope.setAddressName = function(){
+            if($scope.address.Street1){
+                $scope.address.AddressName = $scope.address.Street1;
             }
-        );
-    };
-    $scope.delete = function() {
-        Address.delete(this.address, function() {
-            $location.path($scope.return);
-        });
-    };
+        };
 
-	$scope.cancel = function() {
-		$scope.return ? $location.path($scope.return) : $rootScope.$broadcast('event:AddressCancel');
-	};
+        $scope.save = function() {
+            $scope.objectExists = false;
+            Address.save(this.address,
+                function(address) {
+                    $rootScope.$broadcast('event:AddressSaved', address);
+                    $location.path($scope.return);
+                },
+                function(ex) {
+                    if (ex.Code.is('ObjectExistsException'))
+                        $scope.objectExists = true;
+                }
+            );
+        };
+        $scope.delete = function() {
+            Address.delete(this.address, function() {
+                $location.path($scope.return);
+            });
+        };
 
-    $scope.countries = Resources.countries;
-    $scope.states = Resources.states;
+        $scope.cancel = function() {
+            $scope.return ? $location.path($scope.return) : $rootScope.$broadcast('event:AddressCancel');
+        };
 
-    $scope.country = function(item) {
-        return $scope.address != null ? $scope.address.Country == item.country : false;
-    };
-    $scope.hasStates = function() {
-        return $scope.address != null ? $scope.address.Country == 'US' || $scope.address.Country == 'CA' || $scope.address.Country == 'NL' : false;
-    };
+        $scope.countries = Resources.countries;
+        $scope.states = Resources.states;
 
-    $scope.isPhoneRequired = function() {
-        return ($scope.user.Permissions.contains('BillingAddressPhoneRequired') && $scope.address.IsBilling) ||
-            ($scope.user.Permissions.contains('ShipAddressPhoneRequired') && $scope.address.IsShipping);
-    }
-}]);
+        $scope.country = function(item) {
+            return $scope.address != null ? $scope.address.Country == item.country : false;
+        };
+        $scope.hasStates = function() {
+            return $scope.address != null ? $scope.address.Country == 'US' || $scope.address.Country == 'CA' || $scope.address.Country == 'NL' : false;
+        };
+
+        $scope.isPhoneRequired = function() {
+            return ($scope.user.Permissions.contains('BillingAddressPhoneRequired') && $scope.address.IsBilling) ||
+                ($scope.user.Permissions.contains('ShipAddressPhoneRequired') && $scope.address.IsShipping);
+        }
+    }]);
--- js/controllers/awardsCtrl.js
+++ js/controllers/awardsCtrl.js
@@ -1,57 +1,58 @@
 four51.app.controller('AwardsCtrl', ['$scope', '$location', '$sce', 'User', 'SpendingAccount', 'Order', 'Address', 'GiftCard', 'ProductDisplayService',
-function ($scope, $location, $sce, User, SpendingAccount, Order, Address, GiftCard, ProductDisplayService) {
-	$scope.Order = {};
-	$scope.Order.LineItems = [];
-	$scope.LineItem = {};
-	$scope.Order.BudgetAccount = {};
-	$scope.Order.ShipAddress = { Country: 'US', IsShipping: true, IsBilling: false };
-	
-	function setupOrder() {
-		$scope.Order.BudgetAccountID = $scope.BudgetAccount.ID;
-		$scope.Order.PaymentMethod = 'BudgetAccount';	
-	}
-	
-	function saveOrder() {
-		$scope.Order.LineItems.push($scope.LineItem);
-		Order.submit($scope.Order,
-	        function(data) {
-		        $scope.Order = data;
-	        },
-	        function(ex) {
-		        $scope.error = ex.Message;
-	        }
-        );
-	}
-	
-	$scope.redeemGiftCard = function() {
-		$scope.gettingAward = true;
-		$scope.error = null;
-		GiftCard.redeem(this.giftcard,
-			function(card) {
-				$scope.giftcard = card;
-				SpendingAccount.query(function(data) {
-					$scope.BudgetAccount = data[data.length - 1];
-					ProductDisplayService.getProductAndVariant($scope.BudgetAccount.AccountType.Name, null, function(data){
-						$scope.LineItem.Product = data.product;
-						ProductDisplayService.setNewLineItemScope($scope);
-						ProductDisplayService.setProductViewScope($scope);
-						$scope.LineItem.Quantity = 1;
-						$scope.LineItem.LineTotal = 10;
-						setupOrder();
-						$scope.gettingAward = false;
-					});
-				});
-			},
-			function(ex) {
-				$scope.error = ex.Message;
-				$scope.gettingAward = false;
-			}
-		);
-	}
-	
-	$scope.$on('event:AddressSaved', function(event, address) {
-		$scope.Order.ShipAddressID = address.ID;
-		$scope.LineItem.ShipAddressID = address.ID;
-		saveOrder();
-	});
-}]);
+    function ($scope, $location, $sce, User, SpendingAccount, Order, Address, GiftCard, ProductDisplayService) {
+        $scope.Order = {};
+        $scope.Order.LineItems = [];
+        $scope.LineItem = {};
+        $scope.Order.BudgetAccount = {};
+        $scope.Order.ShipAddress = { Country: 'US', IsShipping: true, IsBilling: false };
+
+        function setupOrder() {
+            $scope.Order.BudgetAccountID = $scope.BudgetAccount.ID;
+            $scope.Order.PaymentMethod = 'BudgetAccount';
+        }
+
+        function saveOrder() {
+            $scope.Order.LineItems.push($scope.LineItem);
+            Order.submit($scope.Order,
+                function(data) {
+                    $scope.Order = data;
+                    $scope.Order.ExternalOrderDetailRecipients = $scope.user.Email;
+                },
+                function(ex) {
+                    $scope.error = ex.Message;
+                }
+            );
+        }
+
+        $scope.redeemGiftCard = function() {
+            $scope.gettingAward = true;
+            $scope.error = null;
+            GiftCard.redeem(this.giftcard,
+                function(card) {
+                    $scope.giftcard = card;
+                    SpendingAccount.query(function(data) {
+                        $scope.BudgetAccount = data[data.length - 1];
+                        ProductDisplayService.getProductAndVariant($scope.BudgetAccount.AccountType.Name, null, function(data){
+                            $scope.LineItem.Product = data.product;
+                            ProductDisplayService.setNewLineItemScope($scope);
+                            ProductDisplayService.setProductViewScope($scope);
+                            $scope.LineItem.Quantity = 1;
+                            $scope.LineItem.LineTotal = 10;
+                            setupOrder();
+                            $scope.gettingAward = false;
+                        });
+                    });
+                },
+                function(ex) {
+                    $scope.error = ex.Message;
+                    $scope.gettingAward = false;
+                }
+            );
+        }
+
+        $scope.$on('event:AddressSaved', function(event, address) {
+            $scope.Order.ShipAddressID = address.ID;
+            $scope.LineItem.ShipAddressID = address.ID;
+            saveOrder();
+        });
+    }]);
