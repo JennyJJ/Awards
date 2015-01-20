211a8a90c53299bbf57a676428e386378cadc00f
Steve Davis <sdavis@four51.com>
2015-01-20 08:05:21 -0600

    repo cleanup

--- js/app.js
+++ js/app.js
@@ -2,3 +2,8 @@
 
 var four51 = {};
 four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics']);
+
+four51.app.run(['$cookieStore', function($cookieStore) {
+	$cookieStore.remove('user.awards');
+	store.clear();
+}]);
--- js/controllers/awardsCtrl.js
+++ js/controllers/awardsCtrl.js
@@ -1,53 +1,26 @@
-four51.app.controller('AwardsCtrl', ['$scope', '$location', '$sce', 'User', 'SpendingAccount', 'Order', 'Address', 'GiftCard', 'ProductDisplayService', '$window', '$cookieStore',
-    function ($scope, $location, $sce, User, SpendingAccount, Order, Address, GiftCard, ProductDisplayService, $window, $cookieStore) {
-            if (store.get('451Cache.Order')) {
-                $scope.Order = store.get('451Cache.Order');
-                $scope.LineItem = store.get('451Cache.LineItem');
-                ProductDisplayService.setProductViewScope($scope);
-            }
-            else {
-                $scope.Order = {};
-                $scope.Order.LineItems = [];
-                $scope.LineItem = {};
-                $scope.Order.BudgetAccount = {};
-                $scope.Order.ShipAddress = { Country: 'US', IsShipping: true, IsBilling: false };
-            }
+four51.app.controller('AwardsCtrl', ['$scope', '$location', '$sce', 'User', 'SpendingAccount', 'Order', 'Address', 'GiftCard', 'ProductDisplayService',
+    function ($scope, $location, $sce, User, SpendingAccount, Order, Address, GiftCard, ProductDisplayService) {
+        $scope.Order = {};
+        $scope.Order.LineItems = [];
+        $scope.LineItem = {};
+        $scope.Order.BudgetAccount = {};
+        $scope.Order.ShipAddress = { Country: 'US', IsShipping: true, IsBilling: false };
 
         function setupOrder() {
             $scope.Order.BudgetAccountID = $scope.BudgetAccount.ID;
             $scope.Order.PaymentMethod = 'BudgetAccount';
-            store.set('451Cache.Order', $scope.Order);
-            store.set('451Cache.LineItem', $scope.LineItem);
         }
 
         function saveOrder() {
             $scope.Order.LineItems.push($scope.LineItem);
-            $scope.Order.ExternalOrderDetailRecipients = $scope.user.Email;
-            var randomPassword = Math.random();
-            var user = {
-                FirstName: 'Temp',
-                LastName: 'User',
-                Email: 'demomail@four51.com',
-                Username: Math.random(),
-                Password: randomPassword,
-                ConfirmPassword: randomPassword,
-                Active: true,
-                TermsAccepted: '2013-05-29T11:47:51.403',
-                ConvertFromTempUser: true
-            };
-            User.save(user, function(u) {
-                $scope.Order.FromUserID = u.ID;
-                Order.submit($scope.Order,
-                    function (data) {
-                        $scope.Order = data;
-                        store.remove('451Cache.Order');
-                        store.remove('451Cache.LineItem');
-                    },
-                    function (ex) {
-                        $scope.error = ex.Message;
-                    }
-                );
-            });
+            Order.submit($scope.Order,
+                function(data) {
+                    $scope.Order = data;
+                },
+                function(ex) {
+                    $scope.error = ex.Message;
+                }
+            );
         }
 
         $scope.redeemGiftCard = function() {
@@ -74,19 +47,11 @@ four51.app.controller('AwardsCtrl', ['$scope', '$location', '$sce', 'User', 'Spe
                     $scope.gettingAward = false;
                 }
             );
-        };
-
+        }
 
         $scope.$on('event:AddressSaved', function(event, address) {
             $scope.Order.ShipAddressID = address.ID;
             $scope.LineItem.ShipAddressID = address.ID;
             saveOrder();
         });
-        //Clear the Cache
-        if($scope.Four51User.currentUser) {
-            if ($scope.Four51User.currentUser.FirstName == 'Temp') {
-                $cookieStore.remove('user.awards');
-                $window.location.href= $window.location.pathname;
-            }
-        }
     }]);
