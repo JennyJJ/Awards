309af11a92c6e3b7b1f2255c62a5d1d5135efe7e
Steve Davis <sdavis@four51.com>
2015-01-20 08:05:12 -0600

    repo cleanup

--- js/controllers/reportCtrl.js
+++
@@ -1,6 +0,0 @@
-four51.app.controller('ReportCtrl', ['$scope', '$routeParams', '$451', 'SavedReports',
-function($scope, $routeParams, $451, SavedReports) {
-	SavedReports.get($routeParams.id, function(data) {
-		$scope.report = data;
-	});
-}]);
--- js/directives/orderdetails.js
+++
@@ -1,7 +0,0 @@
-four51.app.directive('orderdetails', function() {
-	var obj = {
-		restrict: 'AE',
-		templateUrl: 'partials/controls/orderDetails.html'
-	};
-	return obj;
-});
