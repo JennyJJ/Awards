52858eae5121e260d9af7e21507cf8216937a7d8
Jen Rasmussen <jrasmussen@four51.com>
2015-05-04 11:03:06 -0500

    design changes / cleanup

--- js/controllers/Four51Ctrl.js
+++ js/controllers/Four51Ctrl.js
@@ -5,12 +5,11 @@ function ($scope, $route, $location, $451, User, Order, Security, OrderConfig, C
 	$scope.isAnon = $451.isAnon; //need to know this before we have access to the user object
 	$scope.Four51User = Security;
 
-	//remove for awards
-	/*if ($451.isAnon && !Security.isAuthenticated()){
+	if ($451.isAnon && !Security.isAuthenticated()){
 		User.login(function() {
 			$route.reload();
 		});
-	}*/
+	}
 
     // fix Bootstrap fixed-top and fixed-bottom from jumping around on mobile input when virtual keyboard appears
     if ( $(window).width() < 960 ) {
