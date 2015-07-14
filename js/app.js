81c4cf8153808470bf205ffc0cb0f11ca95e9680
Jen Rasmussen <jrasmussen@four51.com>
2015-07-14 13:45:21 -0500

    TRT-01681 Add Product Zoom

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var four51 = {};
-four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics']);
+four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics',
+	'angulartics.google.analytics', 'OrderCloud-ProductZoom']);
 
 four51.app.run(['$cookieStore', function($cookieStore) {
 	$cookieStore.remove('user.awards');
